3d5792b36580de00ab27b8c12285b356e8851f9c
Amr9876 <amr.aig.2007@gmail.com>
2022-12-27 15:05:26 +0200

    initial commit

---
+++ components/index.js
@@ -0,0 +1,10 @@
+import { CircleButton, RectButton } from "./Button";
+import { SubInfo } from "./SubInfo";
+
+export { default as HomeHeader } from "./HomeHeader";
+export { default as NFTCard } from "./NFTCard";
+export { default as FocusedStatusBar } from "./FocusedStatusBar";
+export { default as DetailsDesc } from "./DetailsDesc";
+export { default as DetailsBid } from "./DetailsBid";
+
+export { CircleButton, RectButton, SubInfo };
